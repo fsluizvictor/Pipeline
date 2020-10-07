04860d243fe310d91cd2fc9fc4bb7d65779cbf0e
fsluizvictor <fs.luizvictor@gmail.com>
2020-10-07 09:32:22 -0300

    pipe finalizado

--- Casos de Teste/ApresentacaoPipeline.asm
+++ Casos de Teste/ApresentacaoPipeline.asm
@@ -7,8 +7,16 @@ lw $t4, 0($t1)
 slt $t5, $t0, $t1
 and $t6, $t0, $t1
 or $t7, $t0, $t1
-add $t2, $t0,$t1
 sw $t2, 0($t1) 
 beq $t0, $t1, main
+
+add $t2,$t0,$t1
+add $t3,$t2,$t2
+add $t4,$t2,$t1
+
+lw $t4, 0($t1)
+add $t7,$t4,$t0
+
+
+
 j main
-01095020 01285822 ac090000 8c0c0000 0109682a 01097024 01097825 1109fff8 08000000
